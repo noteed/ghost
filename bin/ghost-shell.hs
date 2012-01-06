e85ff6ebf790bdc4ccf593b56689fc5929256741
Vo Minh Thu <noteed@gmail.com>
2012-01-07 00:31:49 +0100

    Added a -c option to ghost-shell, wrote down some notes about what happen when using git push against it.

--- bin/ghost-shell.hs
+++ bin/ghost-shell.hs
@@ -7,6 +7,9 @@ import Paths_ghost (version)
 import Data.Version (showVersion)
 import System.Console.CmdArgs.Implicit
 
+import System.FilePath ((</>))
+import System.IO (hPutStrLn, withFile, IOMode(WriteMode))
+
 versionString :: String
 versionString =
   "ghost " ++ showVersion version ++ " Copyright (c) 2012 Vo Minh Thu."
@@ -19,12 +22,21 @@ main = (processCmd =<<) $ cmdArgs $
   &= summary versionString
   &= program "ghost-shell"
 
-data Cmd = Shell
+data Cmd = Shell { shellCommand :: String }
   deriving (Data, Typeable)
 
 shell :: Cmd
 shell = Shell
-  &= help "Execute a restricted shell for Ghost operations."
+  { shellCommand = ""
+    &= typ "COMMAND"
+    &= help "non-interactive command to execute"
+    &= explicit
+    &= name "c"
+  } &= help "Execute a restricted shell for Ghost operations."
 
 processCmd :: Cmd -> IO ()
-processCmd Shell{..} = putStrLn "Ghost shell."
+processCmd Shell{..} = do
+  putStrLn "Ghost shell."
+  withFile ("/home/ghost" </> "ghost-shell.txt") WriteMode $ \h -> do
+    hPutStrLn h $
+      "shellCommand:\n" ++ shellCommand
