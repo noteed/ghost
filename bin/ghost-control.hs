9590db4c2b2011e545361250b863a16476176a86
Vo Minh Thu <noteed@gmail.com>
2012-03-22 21:17:37 +0100

    Added script to create the ghost user, continued the ghost-control init command.

--- bin/ghost-control.hs
+++ bin/ghost-control.hs
@@ -5,10 +5,12 @@
 module Main where
 
 import Paths_ghost (version)
+import Control.Monad (when)
 import Data.Version (showVersion)
 import System.Console.CmdArgs.Implicit
 
-import System.Directory (doesFileExist)
+import System.Directory
+  (copyFile, createDirectoryIfMissing, doesFileExist, getHomeDirectory)
 import System.FilePath ((</>))
 import System.IO (hPutStrLn, withFile, IOMode(WriteMode))
 
@@ -34,7 +36,7 @@ data Cmd =
 
 cmdInit :: Cmd
 cmdInit = Init
-  &= help "Initialize Ghost."
+  &= help "Initialize Ghost (server-side)."
 
 cmdAddUser :: Cmd
 cmdAddUser = AddUser
@@ -51,10 +53,40 @@ cmdAddUser = AddUser
     &= name "u"
     &= name "user"
   } &= help "Add a user to Ghost."
+    &= explicit
+    &= name "add-user"
 
 processCmd :: Cmd -> IO ()
 processCmd Init{..} = do
-  putStrLn "Ghost."
+  putStrLn "Ghost server-side initialization."
+  home <- getHomeDirectory
+  let authorizedKeys = home </> ".ssh" </> "authorized_keys"
+      administratorKeys = home </> "administrator" </> "keys"
+
+  _ <- createDirectoryIfMissing True $ home </> "administrator"
+  _ <- createDirectoryIfMissing True $ home </> "user"
+  _ <- createDirectoryIfMissing True $ home </> "gits"
+  _ <- createDirectoryIfMissing True $ home </> "run/staging/nginx"
+  _ <- createDirectoryIfMissing True $ home </> "run/production/nginx"
+  withFile (home </> "run" </> "nginx.conf") WriteMode $ \h -> do
+    hPutStrLn h $ "include " ++ home ++ "/run/staging/nginx/*.conf;"
+    hPutStrLn h $ "include " ++ home ++ "/run/production/nginx/*.conf;"
+
+  b <- doesFileExist $ authorizedKeys
+  if b
+    then do
+      putStrLn $ "Prior authorized_keys file found, " ++
+        "copying it as administrator key."
+      -- TODO make sure there is a single key in it. For now, it assumes this
+      -- is the key copied by setup-ghost-archlinux.sh.
+      copyFile authorizedKeys administratorKeys
+    else putStrLn $ "No prior authorized_keys file found."
+  refreshAuthorizedKeys
+
+  putStrLn "Ghost server-side initialization complete."
+  putStrLn "Add the following line to your main Nginx configuration"
+  putStrLn "file (inside an `http {..}` block.):"
+  putStrLn $ "  include " ++ home ++ "/run/nginx.conf;"
 
 processCmd AddUser{..} = do
   b <- doesFileExist addUserPublicKeyPath
@@ -64,6 +96,10 @@ processCmd AddUser{..} = do
       putStrLn $ authorizedKeysEntry addUserName content
     else putStrLn $ addUserPublicKeyPath ++ " not found."
 
+-- | Given a username and its public SSH key, return a string
+-- that can be appended to a SSH authorized_keys file.
+-- The entry establishes the binding between a username and its
+-- SSH key, so that ghost-command 'knows' who is running it.
 authorizedKeysEntry:: String -> String -> String
 authorizedKeysEntry username key = concat
   [ "command=\"/home/ghost/bin/ghost-command --user=", username, "\""
@@ -73,3 +109,21 @@ authorizedKeysEntry username key = concat
   , ",no-pty "
   , key
   ]
+
+-- | Similar to `authorizedKeysEntry` but for an administrator
+-- role. The entry does nothing special (i.e. the user can log
+-- in as the ghost user and has full access).
+administratorAuthorizedKeysEntry:: String -> String -> String
+administratorAuthorizedKeysEntry _ key = key
+
+-- | Read keys from ~/administrator/keys and ~/user/ and generate
+-- the ~/.ssh/authorized_keys file, pairing SSH keys and usernames.
+refreshAuthorizedKeys :: IO ()
+refreshAuthorizedKeys = do
+  home <- getHomeDirectory
+  let authorizedKeys = home </> ".ssh" </> "authorized_keys"
+      administratorKeys = home </> "administrator" </> "keys"
+  b <- doesFileExist $ administratorKeys
+  when b $ do
+    copyFile administratorKeys authorizedKeys
+    -- TODO add user keys
---
+++ create-ghost-user.archlinux.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+# Run this script as a regular user, with your public SSH  key as argument.
+# Ensure before running the script a SSH server is running and will accept
+# connections for the ghost user.
+#
+# This creates a ghost user, a few directories in its home, copy the SSH
+# key as authorized_keys then ssh into localhost as the ghost user to
+# call ghost-control init which completes the setup.
+
+# TODO:
+# - test $1 is given and is a public key
+# - test ghost-control exists somewhere
+
+public_key=$1
+
+sudo useradd \
+  --system \
+  --shell /bin/sh \
+  --create-home \
+  --user-group \
+  ghost
+
+sudo mkdir -p /home/ghost/{.ssh,bin,administrator}
+sudo cp `which ghost-control` /home/ghost/bin/
+sudo cp $public_key /home/ghost/.ssh/authorized_keys
+sudo chown -R ghost:ghost /home/ghost/
+ssh ghost@localhost bin/ghost-control init
